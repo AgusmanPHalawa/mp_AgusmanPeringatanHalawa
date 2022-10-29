0a0b53924479135e685280be0566e91221178308
Agusman Peringatan Halawa <mp_AgusmanPeringatanHalawa@example.com>
2022-10-29 17:01:19 +0700

    UTS Agusman P Halawa

---
+++ app/src/main/java/com/example/mp_AgusmanPeringatanHalawa/monitor.java
@@ -0,0 +1,14 @@
+package com.example.mp_AgusmanPeringatanHalawa;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class monitor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitor);
+    }
+}
